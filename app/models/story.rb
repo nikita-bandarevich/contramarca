ee4b90e99d9f04c65d2e24981fe95071f2c5456e
Nikita Bandarevich <127barcelona@gmail.com>
2019-08-20 14:58:13 +0200

    FEAT: created routes and index + show of stories are working..

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,7 @@
 class Story < ApplicationRecord
   belongs_to :user
+  has_many :bookmarks
+  has_many :shares
+  has_many :readings
+  has_many :images
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'stories/index'
-  get 'stories/show'
-  get 'stories/edit'
-  get 'stories/update'
-  get 'stories/new'
-  get 'stories/create'
-  get 'stories/destroy'
+  # get 'stories/index'
+  # get 'stories/show'
+  # get 'stories/edit'
+  # get 'stories/update'
+  # get 'stories/new'
+  # get 'stories/create'
+  # get 'stories/destroy'
+  resources :stories
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
