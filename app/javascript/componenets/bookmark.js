ba168edc5980c31b17f5dbb540d12dae6944f81a
Nikita Bandarevich <127barcelona@gmail.com>
2019-08-27 15:18:31 +0200

    created the beginnibng of js for ajax change of icons

---
+++ app/javascript/componenets/bookmark.js
@@ -0,0 +1,6 @@
+const toggler = () => {
+  x.classList.toggle("far fa-bookmark");
+  x.classList.toggle("fas fa-bookmark");
+ }
+
+export {toggler}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,5 @@
 import "bootstrap";
+
+import { toggler } from '../componenets/bookmark';
+
+toggler();
